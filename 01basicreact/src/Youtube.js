cdd981ac8c66a78a61ddc75eda74f98bb2dafaa2
harshitha <immaneni.harshitha@gmail.com>
2024-01-15 21:53:48 -0800

    start learning react

---
+++ 01basicreact/src/Youtube.js
@@ -0,0 +1,9 @@
+
+
+function Youtube(){
+    return(
+        <h1>youtube react app</h1>
+    )
+}
+
+export default Youtube
---
+++ 01basicvite/src/App.jsx
@@ -0,0 +1,16 @@
+
+import Youtube from "./Youtube"
+
+const username="harshu"
+function App() {
+  
+  return (
+    <>
+    <h1>Vite react app {2+2}</h1>
+    <h2>Vite react app {username}</h2>
+    <Youtube/>
+    </>
+  )
+}
+
+export default App
