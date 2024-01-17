75bf25b0a3e424d59692b572aad95f26c28c5568
harshitha <immaneni.harshitha@gmail.com>
2024-01-17 15:33:16 +0530

    start learning react

--- 07reactRouter/components/Github/Github.jsx
+++ 07reactRouter/components/Github/Github.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
+import { useEffect } from 'react'
+import {useLoaderData} from 'react-router-dom'
 
 function Github() {
+
+    const data= useLoaderData()
+
+    // const [date,setData] = React.useState([])
+    // useEffect(() =>{
+    //     fetch('https://api.github.com/users/hiteshchoudhary')
+    //     .then((Response)=> Response.json())
+    //     .then(data => {
+    //         console.log(data)
+    //         setData(data)
+    //     })
+    // }, [])
+
+    
   return (
-    <div>Github</div>
+    <div className='text-center m-4 bg-gray-600
+    text-white p-4 text-3xl'>Github followers: {data.followers}
+    <img src={data.avatar_url} width={300} alt="" />
+    </div>
   )
 }
 
 export default Github
+
+export const githubInfoLoader = async () => {
+ const Response = await fetch('https://api.github.com/users/hiteshchoudhary')
+ return Response.json()
+
+}
--- 07reactRouter/src/main.jsx
+++ 07reactRouter/src/main.jsx
@@ -7,6 +7,9 @@ import Layout from './Layout.jsx'
 import Home from '../components/Home/Home.jsx'
 import About from '../components/About/About.jsx'
 import User from '../components/User/User.jsx'
+import Github, {githubInfoLoader} from '../components/Github/Github.jsx'
+
+
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -16,6 +19,11 @@ const router = createBrowserRouter(
       <Route path="user/" element={<User />}>
           <Route path=':userid' element={<User />} />
       </Route>
+      <Route 
+      loader={githubInfoLoader}
+      path='github'
+      element={<Github />} />
+      <Route path='*' element={<div>not Found</div>} />
     </Route>
   )  
 )
